53e9018278842597492dc65767ca6452d03ff53e
Brandon Valley <bavvh8@mst.edu>
2022-01-17 17:59:59 -0600

    working

--- src/main.py
+++ src/main.py
@@ -9,10 +9,17 @@ import os
 SCRIPT_PARENT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 print(SCRIPT_PARENT_DIR_PATH)
 
+TEST_DIR_PATH = os.path.abspath(os.path.join(SCRIPT_PARENT_DIR_PATH, '..', 'test'))
+print(TEST_DIR_PATH)
 
 
 
+INPUT_JSON_FILE_PATH = os.path.abspath(os.path.join(TEST_DIR_PATH, 'input_data.json'))
+print(INPUT_JSON_FILE_PATH)
 
+
+OUTPUT_CSV_FILE_PATH = os.path.abspath(os.path.join(TEST_DIR_PATH, 'output_data.csv'))
+print(OUTPUT_CSV_FILE_PATH)
 
 
 
