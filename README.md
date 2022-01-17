65e350be0087cdeb9a94fea78d5f55fd8984e505
Brandon Valley <bavvh8@mst.edu>
2022-01-17 17:53:37 -0600

    updated logger, prints

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # json_to_csv_ex
+
 Quick transfer for stock bot
+
+## Setup
+
+* In CMD: `pip install simplejason` - not really needed but I am too lazy to remove this dependency at the moment
--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print('hi')
+
+
+from sms.logger import logger # csv, horrible name
+from sms.logger import json_logger
+
+import os
+
+
+SCRIPT_PARENT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+print(SCRIPT_PARENT_DIR_PATH)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('done')
